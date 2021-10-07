e49981d8f21e4a6d7190faabe9735aa6ec713abd
Андрей Щукин <1929999@gmail.com>
2021-10-07 12:48:09 +0300

    HomeWork2

---
+++ Asteroids/Rocket.cs
@@ -0,0 +1,22 @@
+﻿using Asteroids.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    class Rocket : Asteroid
+    {
+        public Rocket(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            
+        }
+        public override void SetImages()
+        {
+            images = new Image[1] { Resources.rocket };
+        }
+    }
+}
